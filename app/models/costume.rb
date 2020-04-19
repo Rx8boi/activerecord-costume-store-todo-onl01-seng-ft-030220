2b2723f9d0b13bdc2a9b1e8e5bc165cc12b66f30
Charles Butler <cdbutle@outlook.com>
2020-04-19 23:50:24 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,4 +3,13 @@
 
 class Costume < ActiveRecord::Base
 
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.text :image_url
+      timestamp :created_at
+      timestamp :updated_at
+    end
 end
