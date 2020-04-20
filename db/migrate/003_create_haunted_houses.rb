09838788cacb92cbea5e9398d4bb7e0c52091760
Charles Butler <cdbutle@outlook.com>
2020-04-20 00:06:32 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,17 +1,1 @@
 # Create your haunted_houses migration here
-
-class HauntedHouse < ActiveRecord::Migration[4.2]
-
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family
-      t.datetime :opening
-      t.datetime :closing
-      t.text :long_description
-    end
-  end
-end
