f1b4774164a8875a2d9b22b389816e1425b0d6b9
Charles Butler <cdbutle@outlook.com>
2020-04-20 00:05:00 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,15 +2,4 @@
 # It should inherit from ActiveRecord::Base
 
 class Costume < ActiveRecord::Base
-
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.string :size
-      t.text :image_url
-      timestamp :created_at
-      timestamp :updated_at
-    end
-  end
 end
