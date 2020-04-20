ea3062c8da38f07c522ae87f8ee17b91e61c3418
Charles Butler <cdbutle@outlook.com>
2020-04-20 00:06:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,15 +1,2 @@
 # Create your costume_stores migration here
 
-class costume_store
-  def change
-    create_table :costume_stores do |t|
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :employees
-      t.boolean :in_business?
-      t.datetime :opening_time
-      t.datetime :closing_time
-    end
-  end
-end
